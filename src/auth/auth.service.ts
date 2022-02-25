b8a08d5d1a5d97090db3e502dc0eb740b53ecfd1
Babatunde Lamidi <odunayohlamidi@gmail.com>
2022-02-25 08:15:32 +0100

    chore: prisma docker

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  login() {
+    return { message: 'I am login' };
+  }
+
+  signup() {
+    return { message: 'I am signup' };
+  }
+}
